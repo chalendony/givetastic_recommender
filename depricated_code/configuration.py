5b94f743aff6918888e044e54024825b700ebbeb
Avaré Stewart <avare.stewart@gmail.com>
2020-10-30 18:01:57 +0100

    update readme

--- depricated_code/configuration.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-root_name, _, _ = __name__.partition('.')
-root_module = sys.modules[root_name]
-root_dir = os.path.dirname(root_module.__file__)
-
-config_path = os.path.join(root_dir, 'configuration.py')
